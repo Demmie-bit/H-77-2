7c2b91e22cf7057dfd164a88a0a0a83fc4a7239e
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2021-06-29 12:24:17 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+var first_name= "Anwesha ";
+var last_name= "Paul";
+console.log(first_name.concat(last_name));
+var Last_name= "Paul";
+
+var number1= "10";
+var number2= "5";
+console.log(number1);
+console.log(number2);
+var add= number1 + number2 ;
+console.log(add);
+var substract= number1 - number2;
+console.log(substract);
+  
+
